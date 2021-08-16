539a971aec85ab2e820c8ea0f45db5dce0d80aed
JackyJung <jung.youngseok@gmail.com>
2021-08-16 20:52:24 +0900

    Update DomainCrawlerService.java
    adding custom span when producing message

--- domain-crawler/src/main/java/com/techprimers/domaincrawler/DomainCrawlerService.java
+++ domain-crawler/src/main/java/com/techprimers/domaincrawler/DomainCrawlerService.java
@@ -1,10 +1,16 @@
 package com.techprimers.domaincrawler;
+import datadog.trace.api.Trace;
 
 import org.springframework.http.MediaType;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import datadog.trace.api.DDTags;
+import io.opentracing.Scope;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
 
 @Service
 public class DomainCrawlerService {
@@ -16,8 +22,11 @@ public class DomainCrawlerService {
     this.kafkaTemplate = kafkaTemplate;
   }
 
+  @Trace(operationName = "crawl.service", resourceName = "DomainCrawlerService.crawl")
   public void crawl(String name) {
 
+      // Tags can be set when creating the span
+	  
     Mono<DomainList> domainListMono = WebClient.create()
         .get()
         .uri("https://api.domainsdb.info/v1/domains/search?domain=" + name + "&zone=com")
@@ -25,14 +34,33 @@ public class DomainCrawlerService {
         .retrieve()
         .bodyToMono(DomainList.class);
 
-
     domainListMono.subscribe(domainList -> {
+
       domainList.domains
           .forEach(domain -> {
+              Tracer tracer = GlobalTracer.get();
+              Span span = tracer.buildSpan("crawl.service")
+                      .withTag(DDTags.SERVICE_NAME, "domain-crawler")
+                      .withTag(DDTags.RESOURCE_NAME, "DomainCrawlerService.crawl")
+                      .start();
+                  try (Scope scope = tracer.activateSpan(span)) {
+                      // Tags can also be set after creation
+                      span.setTag("domain", domain.getDomain());
+
+                      // The code you’re tracing
+
+                  } catch (Exception e) {
+                      // Set error on span
+                  } finally {
+                      // Close span in a finally block
+                      span.finish();
+                  }
+
             kafkaTemplate.send(KAFKA_TOPIC, domain);
             System.out.println("Domain message" + domain.getDomain());
           });
     });
 
   }
+  
 }
